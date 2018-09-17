fb4d4d256d05d1d6288e58325cbb95e24c6ad85b
Juventino Perez <juventus139@hotmail.com>
2018-09-17 06:00:51 +0000

    feat: The journey begins, done

---
+++ arcade_juv/centuryFromYear.rb
@@ -0,0 +1,7 @@
+def centuryFromYear(year)
+
+    return 1 + (year-1)/100
+##1905 -1 = 1904/100 19.04 +1 = 20000000000
+
+end def centuryFromYear(year)
+
---
+++ arcade_juv/intro.rb
@@ -0,0 +1,5 @@
+########### intro 
+def add (param1 , param2) 
+    param1+param2 
+
+end 
